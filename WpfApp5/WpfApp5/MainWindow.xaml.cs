628403d9b042fd81b06f590261a332d44d8e4f14
vitfro <46087338+vitfro@users.noreply.github.com>
2018-12-24 00:44:50 +0300

    Add files via upload

---
+++ WpfApp5/WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double x = 1;
+            double n = Convert.ToInt64(Vivod1.Text);
+            for (long i = 2; i <= n; i++)
+            {
+                x *= i;
+            }
+            Vivod2.Text = x.ToString();
+        }
+    }
+}
